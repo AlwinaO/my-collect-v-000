3ff7b1e24ceffba9eb4fa0c6e586cca87ee918a4
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-06 00:36:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,14 @@
-def my_collect
-  
+def my_collect(array)
+  i = 0
+    collection = []
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    collection
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
 end
